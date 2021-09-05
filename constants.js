e4560403ee1c8a5cf9f6e0a2a18f713cf1103cc9
ZShadow01 <57576006+ZShadow01@users.noreply.github.com>
2021-09-05 17:28:30 +0200

    Add files via upload
    Added executable commands only with certain permissions + Kick/Ban/Improved Purge

---
+++ constants.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+
+const PERM_FLAGS = Discord.Permissions.FLAGS;
+const INTENT_FLAGS = Discord.Intents.FLAGS;
+
+module.exports = {permFlags: PERM_FLAGS, intentFlags: INTENT_FLAGS};
--- modules/moderation.js
+++ modules/moderation.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { permFlags } = require('../constants.js');
 
 
 const commands = [
@@ -16,9 +17,10 @@ const commands = [
         name: "purge",
         aliases: ['clean'],
         usage: "<amount>",
+        permissions: [permFlags.MANAGE_MESSAGES],
         execute(client, message, args) {
             const limit = parseInt(args[0]);
-            if (!isNaN(limit)) message.channel.bulkDelete(limit + 1);
+            if (!isNaN(limit)) message.channel.bulkDelete(limit < 100 ? limit + 1: limit);
             else message.channel.send(`\`${args[0]}\` is not a number`);
         }
     },
@@ -27,8 +29,32 @@ const commands = [
     {
         name: "ban",
         usage: "<member> [reason]",
+        permissions: [permFlags.BAN_MEMBERS],
         execute (client, message, args) {
-            message.channel.send("Banning!!!!");
+            const member = message.mentions.members.first();
+            if (!member) return message.channel.send("No member mentioned");
+
+            if (!member.toString().replace('!', '') === args[0].replace('!', ''))
+                return message.channel.send("First argument has to be a <member>");
+
+            const reason = args.slice(1).join(' ').toString();
+            message.guild.members.ban(member.id.toString(), {reason: reason.length > 0 ? reason : null});
+        }
+    },
+
+    // Kick
+    {
+        name: "kick",
+        usage: "<member> [reason]",
+        execute(client, message, args) {
+            const member = message.mentions.members.first();
+            if (!member) return message.channel.send("No member mentioned");
+
+            if (!member.toString().replace('!', '') === args[0].replace('!', ''))
+                return message.channel.send("First argument has to be a <member>");
+
+            const reason = args.slice(1).join(' ').toString();
+            message.guild.members.kick(member.id.toString(), {reason: reason.length > 0 ? reason : null});
         }
     }
 ];
